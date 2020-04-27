f3167d0e79dec61fcd716aae39644693dbcbcf99
rogersoh <jaggedfel2560@gmail.com>
2020-04-27 16:24:27 +0800

    3rd commit

--- iot lora training/dht22_ldr_lora_client/dht22_ldr_lora_client.ino
+++ iot lora training/dht22_ldr_lora_client/dht22_ldr_lora_client.ino
@@ -6,16 +6,16 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 int sensorPin = A0;
-int battPin = A7;
+int battPin = A3;
 int sensorValue = 0;
 float battValue = 0.0;
 int battRaw = 0;
 String deviceID = "S2";  //device identification
-/*
-  int myval;
-  byte payload[4];
-  int payloadsize = 4;
-*/
+
+int myval;
+byte payload[8];
+int payloadsize = 8;
+
 
 //for arduino uno lora shield
 #define RFM95_CS 10
@@ -75,19 +75,25 @@ void loop()
   for (int i = 0; i < 10; i++) {
     battRaw += analogRead(battPin);
   }
-  battValue = battRaw / (1024.00 * 10) * 3.3 * 2;
+  battValue = battRaw / (1024.00 * 10) * 5 * 2;
 
 
-  /*myval = t * 100;
-    payload[0] = highByte(myval);
-    payload[1] = lowByte(myval);
-    myval = h * 100;
-    payload[2] = highByte(myval);
-    payload[3] = lowByte(myval);
-    decode payload
+  myval = t * 100;
+  payload[0] = highByte(myval);
+  payload[1] = lowByte(myval);
+  myval = h * 100;
+  payload[2] = highByte(myval);
+  payload[3] = lowByte(myval);
+  /*decode payload
     myval = (payload[0] << 8) + payload[1];
     float temp = myval / 100.0;
   */
+  myval = sensorValue;
+  payload[4] = highByte(myval);
+  payload[5] = lowByte(myval);
+  myval = battValue*100;
+  payload[6] = highByte(myval);
+  payload[7] = lowByte(myval);
 
   String d = "{\"ID\": \"" + String (deviceID) + "\"" + ",";
   d += "\"Temp\":" + String (t) + ",";
@@ -96,18 +102,19 @@ void loop()
   d += "\"Battery\":" + String (battValue);
   d += "} ";  // Add a trailing space is necessary
 
-  /* convert dec to byte
-    String e = deviceID;
-    for (int i = 0; i < payloadsize; i++) {
-     e += "," + String (payload[i], 16);
-    }*/
+  // convert dec to byte
+  String e = deviceID;
+  for (int i = 0; i < payloadsize; i++) {
+    e += "," + String (payload[i], 16);
+  }
+  e +=" ";
   Serial.println(d);
   //Serial.println(e);
   delay(1000);
 
-  char data[d.length()];
-  d.toCharArray(data, d.length());
-  Serial.println(d);
+  char data[e.length()];
+  e.toCharArray(data, e.length());
+  Serial.println(e);
 
   rf95.send((uint8_t*)data, sizeof(data));
 
